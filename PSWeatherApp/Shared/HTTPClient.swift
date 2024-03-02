3f48588bf3a7d6a4e86fb0039c60d3f06976f473
Mustafa Kemal GÖKÇE <mkemalgokce10@gmail.com>
2024-03-03 00:29:49 +0300

    Update HTTPClient protocol

--- PSWeatherApp/Shared/HTTPClient.swift
+++ PSWeatherApp/Shared/HTTPClient.swift
@@ -7,6 +7,15 @@
 
 import Foundation
 
-protocol HTTPClient {
-    func get(from url: URL, completion: @escaping (Result<(Data, HTTPURLResponse), Error>) -> Void)
+public protocol HTTPClientTask {
+    func cancel()
+}
+
+public protocol HTTPClient {
+    typealias Result = Swift.Result<(Data, HTTPURLResponse), Error>
+    
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate threads, if needed.
+    @discardableResult
+    func get(from url: URL, completion: @escaping (Result) -> Void) -> HTTPClientTask
 }
