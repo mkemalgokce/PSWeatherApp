a488a4502bb7c0519335e49659d3504958a1f0c2
Mustafa Kemal GÖKÇE <mkemalgokce10@gmail.com>
2024-03-02 16:46:47 +0300

    Update RemoteWeatherLoader load logic

--- PSWeatherApp/Weather API/RemoteWeatherLoader.swift
+++ PSWeatherApp/Weather API/RemoteWeatherLoader.swift
@@ -11,6 +11,8 @@ final class RemoteWeatherLoader: WeatherLoader {
     private let url: URL
     private let client: HTTPClient
     
+    typealias Result = LoadWeatherResult
+    
     init(url: URL, client: HTTPClient) {
         self.url = url
         self.client = client
@@ -21,7 +23,24 @@ final class RemoteWeatherLoader: WeatherLoader {
         case connectivity
     }
     
-    func load(completion: @escaping (LoadWeatherResult) -> Void) {
-        
+    func load(completion: @escaping (Result) -> Void) {
+        client.get(from: url) { [weak self] result in
+            guard self != nil else { return }
+            switch result {
+                case .success((let data, let response)):
+                    completion(Self.map(data, from: response))
+                case .failure:
+                    completion(.failure(Error.connectivity))
+            }
+        }
+    }
+    
+    static func map(_ data: Data, from response: HTTPURLResponse) -> Result {
+        do {
+            let weathers = try WeatherItemsMapper.map(data, from: response)
+            return .success(weathers)
+        } catch {
+            return .failure(error)
+        }
     }
 }
