5fa8589f073d29cca6c3a181f65bb3906b8b10ae
Mustafa Kemal GÖKÇE <mkemalgokce10@gmail.com>
2024-03-02 16:44:46 +0300

    Create RemoteWeatherLoader

--- PSWeatherApp/Weather API/RemoteWeatherLoader.swift
+++ PSWeatherApp/Weather API/RemoteWeatherLoader.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+final class RemoteWeatherLoader: WeatherLoader {
+    private let url: URL
+    private let client: HTTPClient
+    
+    init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    enum Error: Swift.Error {
+        case invalidData
+        case connectivity
+    }
+    
+    func load(completion: @escaping (LoadWeatherResult) -> Void) {
+        
+    }
+}
