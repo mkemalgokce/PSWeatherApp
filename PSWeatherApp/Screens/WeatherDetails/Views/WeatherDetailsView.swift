d974c276e1ecd4bf5b230aa0db2d022e42a0f195
Mustafa Kemal GÖKÇE <mkemalgokce10@gmail.com>
2024-03-03 18:00:43 +0300

    Update WeatherDetailsView

--- PSWeatherApp/Screens/WeatherDetails/Views/WeatherDetailsView.swift
+++ PSWeatherApp/Screens/WeatherDetails/Views/WeatherDetailsView.swift
@@ -5,4 +5,147 @@
 //  Created by Mustafa Kemal Gökçe on 3.03.2024.
 //
 
-import Foundation
+import UIKit
+
+final class WeatherDetailsView: UIView {
+    private let humidityTitle: String = "Humidity"
+    private let windSpeedTitle: String = "Wind Speed"
+    
+    private let weatherImageView: UIImageView = {
+        let view = UIImageView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius = 25
+        view.image = UIImage(resource: .rainShowers)
+        view.contentMode = .scaleAspectFit
+
+        return view
+    }()
+    
+    private let temperatureLabel: PSHeaderLabel = {
+        let label = PSHeaderLabel()
+        label.text = "36°"
+        label.textAlignment = .center
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 72, weight: .bold)
+        return label
+    }()
+    
+    private let windSpeedInfoView: PSWeatherInfoItemView = {
+        let item = PSWeatherInfoItemView(title: "Wind Speed: 14.6", image: UIImage(systemName: "wind"))
+        item.labelFont = .systemFont(ofSize: 34, weight: .heavy)
+        item.labelColor = .customTitle
+        return item
+    }()
+    
+    private let humidityInfoView: PSWeatherInfoItemView = {
+        let item = PSWeatherInfoItemView(title: "Humidity: 14.6", image: UIImage(systemName: "humidity"))
+        item.labelFont = .systemFont(ofSize: 34, weight: .heavy)
+        item.labelColor = .customTitle
+        return item
+    }()
+    
+    private let weatherDescriptionLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 18, weight: .bold)
+        label.text = "Heavy Rainy"
+        label.textColor = .customTitle
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let topHStack: UIStackView = {
+        let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .horizontal
+        return stack
+    }()
+        
+    let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 0
+
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.isPagingEnabled = true
+        collectionView.showsHorizontalScrollIndicator = false
+      
+        return collectionView
+        
+    }()
+    
+    override var backgroundColor: UIColor? {
+        didSet {
+            collectionView.backgroundColor = backgroundColor
+        }
+    }
+    
+    init() {
+        super.init(frame: .zero)
+        setupView()
+        setupLayout()
+        backgroundColor = .customBackground
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+        addSubview(topHStack)
+        
+
+        topHStack.addArrangedSubview(weatherImageView)
+        topHStack.addArrangedSubview(temperatureLabel)
+
+        addSubview(weatherDescriptionLabel)
+        addSubview(windSpeedInfoView)
+        addSubview(humidityInfoView)
+        
+        addSubview(collectionView)
+
+        
+    }
+    
+    private func setupLayout() {
+        NSLayoutConstraint.activate([
+            
+            weatherImageView.widthAnchor.constraint(equalTo: safeAreaLayoutGuide.widthAnchor, multiplier: 1/2),
+        
+            topHStack.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            
+            topHStack.widthAnchor.constraint(equalTo: safeAreaLayoutGuide.widthAnchor),
+            topHStack.heightAnchor.constraint(equalTo:topHStack.widthAnchor, multiplier: 0.5),
+            
+            weatherDescriptionLabel.topAnchor.constraint(equalTo: topHStack.bottomAnchor, constant: 16),
+            weatherDescriptionLabel.centerXAnchor.constraint(equalTo: centerXAnchor, constant: 16),
+            
+            windSpeedInfoView.topAnchor.constraint(equalTo: weatherDescriptionLabel.bottomAnchor, constant: 16),
+            windSpeedInfoView.centerXAnchor.constraint(equalTo: safeAreaLayoutGuide.centerXAnchor),
+            
+            humidityInfoView.topAnchor.constraint(equalTo: windSpeedInfoView.bottomAnchor, constant: 16),
+            humidityInfoView.centerXAnchor.constraint(equalTo: safeAreaLayoutGuide.centerXAnchor),
+            
+            humidityInfoView.widthAnchor.constraint(equalTo: windSpeedInfoView.widthAnchor),
+            
+            collectionView.heightAnchor.constraint(equalToConstant: 250),
+            collectionView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -16),
+            collectionView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 8),
+            collectionView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -8),
+        ])
+    }
+    
+    func configure(with weather: Weather) {
+        temperatureLabel.text = "\(weather.temperature)"
+        
+        humidityInfoView.text = "\(humidityTitle): \(weather.humidity)"
+        windSpeedInfoView.text = "\(windSpeedTitle): \(weather.windSpeed)"
+        
+        weatherDescriptionLabel.text = weather.weatherDescription.rawValue.capitalizedFirstLetterOfEachWord()
+        weatherImageView.image = weather.weatherDescription.image()
+        backgroundColor = weather.weatherDescription.color()
+        
+    }
+}
+
--- PSWeatherApp/Screens/WeatherDetails/Views/WeatherDetailsViewController.swift
+++ PSWeatherApp/Screens/WeatherDetails/Views/WeatherDetailsViewController.swift
@@ -8,8 +8,80 @@
 import UIKit
 
 final class WeatherDetailsViewController: UIViewController {
+    private let viewModel: WeatherDetailViewModel
+    
+    private let weatherDetailsView = WeatherDetailsView()
+    
+    private var collectionView: UICollectionView {
+        weatherDetailsView.collectionView
+    }
+    
+    init(viewModel: WeatherDetailViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func loadView() {
+        view = weatherDetailsView
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = viewModel.title
+        weatherDetailsView.configure(with: viewModel.weather)
+        navigationController?.navigationBar.prefersLargeTitles = true
+        setupCollectionView()
     }
+    
+    private func setupCollectionView() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(WeatherDetailsForecastCollectionViewCell.self,
+                                forCellWithReuseIdentifier: WeatherDetailsForecastCollectionViewCell.identifier)
+
+    }
+    
+}
+
+//MARK: - UICollectionViewDelegate & UICollectionViewDataSource methods
+extension WeatherDetailsViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        viewModel.foreCastCount
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: WeatherDetailsForecastCollectionViewCell.identifier, for: indexPath) as? WeatherDetailsForecastCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        cell.backgroundColor = viewModel.weather.weatherDescription.color()
+        cell.configure(with: viewModel.forecast(at: indexPath))
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: 200, height: 250)
+    }
+    
+}
+
+@available(iOS 17, *)
+#Preview {
+    UINavigationController(rootViewController: WeatherDetailsViewController(
+        viewModel: WeatherDetailViewModel(weather: Weather(id: 1,
+                                                           city: "Istanbul",
+                                                           country: "Turkey",
+                                                           temperature: 21.2,
+                                                           weatherDescription: .cloudy,
+                                                           humidity: 12,
+                                                           windSpeed: 23, forecast: [
+                                                            Forecast(date: Date.now.description, temperature: 23, weatherDescription: .sunny, humidity: 12, windSpeed: 23),
+                                                            Forecast(date: Date.now.description, temperature: 25, weatherDescription: .rain, humidity: 12, windSpeed: 23),
+                                                            Forecast(date: Date.now.description, temperature: 23, weatherDescription: .sunny, humidity: 12, windSpeed: 23),
+                                                           
+                                                           ]))
+    ))
 }
