33f13773baf25cdc554515c6faaab55ec01a3489
Mustafa Kemal GÖKÇE <mkemalgokce10@gmail.com>
2024-03-03 14:16:28 +0300

    Update CoreDataWeatherStore

--- PSWeatherApp/Weather Cache/CoreDataWeatherStore.swift
+++ PSWeatherApp/Weather Cache/CoreDataWeatherStore.swift
@@ -9,17 +9,73 @@ import Foundation
 import CoreData
 
 final class CoreDataWeatherStore: WeatherStore {
+    
+    enum Error: Swift.Error {
+        case cacheNotFound
+    }
+    
+    let persistentContainer: NSPersistentContainer = {
+        
+        let container = NSPersistentContainer(name: "WeatherDataModel")
+        container.loadPersistentStores { (storeDescription, error) in
+            if let error = error {
+                fatalError("Loading of store failed \(error)")
+            }
+        }
+        
+        print("Container loaded. \(container.persistentStoreDescriptions)")
+        return container
+    }()
+    
+    
+    private var context: NSManagedObjectContext {
+        persistentContainer.viewContext
+    }
+    
+    
     func deleteCachedWeather() throws {
+        try context.performAndWait {
+            let cacheEntities = try context.fetch(CacheEntity.fetchRequest())
+            
+            for cacheEntity in cacheEntities {
+                guard let weathers = cacheEntity.weather else { continue }
+                for weather in weathers {
+                    if let weatherEntity = weather as? WeatherEntity {
+                        try WeatherEntity.delete(weatherEntity, context: context)
+                    }
+                }
+            }
+            
+            try context.save()
+        }
         
     }
     
-    func insert(_ weather: [Weather], timestamp: Date) throws {
+    func insert(_ weathers: [Weather], timestamp: Date) throws {
+        
+        try context.performAndWait {
+            let cache = CacheEntity(context: context)
+            cache.timestamp = timestamp
+            
+            for weather in weathers {
+                cache.addToWeather(WeatherEntity.create(from: weather, context: context))
+            }
+            
+            try context.save()
+        }
         
     }
     
     func retrieve() throws -> CachedWeather? {
+        let fetchRequest: NSFetchRequest<CacheEntity> = CacheEntity.fetchRequest()
+        
+        let result = try context.fetch(fetchRequest)
+        
+        if let first = result.first {
+            return first.toCachedWeather()
+        }
+        
         return nil
     }
-    
     
 }
