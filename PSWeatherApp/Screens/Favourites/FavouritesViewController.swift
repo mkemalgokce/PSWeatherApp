34fac9f00d4530dd27b5bab9b03a71fd8284f008
Mustafa Kemal GÖKÇE <mkemalgokce10@gmail.com>
2024-03-03 20:05:20 +0300

    Update resources

---
+++ PSWeatherApp/Screens/Favourites/FavouritesViewController.swift
@@ -0,0 +1,8 @@
+//
+//  FavouritesViewController.swift
+//  PSWeatherApp
+//
+//  Created by Mustafa Kemal Gökçe on 3.03.2024.
+//
+
+import Foundation
---
+++ PSWeatherApp/Screens/Utils/UIView+Gradient.swift
@@ -0,0 +1,8 @@
+//
+//  UIView+Gradient.swift
+//  PSWeatherApp
+//
+//  Created by Mustafa Kemal Gökçe on 3.03.2024.
+//
+
+import Foundation
