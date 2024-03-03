f4d615c6e258ae614fc5e2e50d694570311a6100
Mustafa Kemal GÖKÇE <mkemalgokce10@gmail.com>
2024-03-03 18:00:28 +0300

    Create WeatherDetailsViewModel

--- PSWeatherApp/Screens/WeatherDetails/ViewModels/WeatherDetailViewModel.swift
+++ PSWeatherApp/Screens/WeatherDetails/ViewModels/WeatherDetailViewModel.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+final class WeatherDetailViewModel {
+    var weather: Weather
+    
+    var title: String {
+        "\(weather.country) / \(weather.city)"
+    }
+    
+    var foreCastCount: Int {
+        weather.forecast.count
+    }
+    
+    init(weather: Weather) {
+        self.weather = weather
+    }
+    
+    func favourite() {
+        
+    }
+    
+    func forecast(at index: IndexPath) -> Forecast {
+        weather.forecast[index.item]
+    }
+    
+}
