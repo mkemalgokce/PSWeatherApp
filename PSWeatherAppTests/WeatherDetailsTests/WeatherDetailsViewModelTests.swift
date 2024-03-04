f8f676a65536dc4526e61fb3125d84af996a81a4
Mustafa Kemal GÖKÇE <mkemalgokce10@gmail.com>
2024-03-04 13:48:08 +0300

    Create WeatherDetails tests

--- PSWeatherAppTests/WeatherDetailsTests/WeatherDetailsViewModelTests.swift
+++ PSWeatherAppTests/WeatherDetailsTests/WeatherDetailsViewModelTests.swift
@@ -5,4 +5,71 @@
 //  Created by Mustafa Kemal Gökçe on 4.03.2024.
 //
 
-import Foundation
+import XCTest
+@testable import PSWeatherApp
+
+class WeatherDetailViewModelTests: XCTestCase {
+    
+    private var viewModel: WeatherDetailViewModel!
+    private var favouriteManager: MockFavouriteManager!
+    private var weather: Weather!
+    
+    override func setUpWithError() throws {
+        favouriteManager = MockFavouriteManager()
+        weather = anyWeather()
+        viewModel = WeatherDetailViewModel(weather: weather, favouriteManager: favouriteManager)
+    }
+    
+    override func tearDownWithError() throws {
+        favouriteManager = nil
+        viewModel = nil
+        weather = nil
+    }
+    
+    func test_forecastCount() {
+        let expectedCount = 0
+        XCTAssertEqual(viewModel.foreCastCount, expectedCount)
+    }
+    
+    func test_isInFavourite() {
+        XCTAssertFalse(viewModel.isInFavourite)
+    }
+    
+    func test_addToFavouritesSuccess() {
+        let expectation = self.expectation(description: "Add to favourites successful")
+        
+        viewModel.addToFavourites()
+        
+        XCTAssertEqual(favouriteManager.savedWeather, weather)
+        XCTAssertTrue(viewModel.isInFavourite)
+        expectation.fulfill()
+        
+
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+    
+    private class MockFavouriteManager: FavouriteManagerProtocol {
+        var savedWeather: Weather?
+        var mockIsInStoreResult: Bool = false
+        
+        func save(weather: Weather) throws {
+            savedWeather = weather
+        }
+        
+        func delete(weather: Weather) throws {
+            savedWeather = nil
+        }
+        
+        func isInStore(_ weather: Weather) throws -> Bool {
+            savedWeather == weather
+        }
+        
+        func load(completion: @escaping (Result<[PSWeatherApp.Weather], Error>) -> Void) {
+            
+        }
+    }
+    
+}
+
+
+
--- PSWeatherAppTests/WeatherListTests/WeatherListViewControllerTests.swift
+++ PSWeatherAppTests/WeatherListTests/WeatherListViewControllerTests.swift
@@ -5,4 +5,85 @@
 //  Created by Mustafa Kemal Gökçe on 4.03.2024.
 //
 
-import Foundation
+import XCTest
+@testable import PSWeatherApp
+
+class WeatherListViewControllerTests: XCTestCase {
+    
+    var viewController: WeatherListViewController!
+    var viewModel: WeatherListViewModel!
+    var weatherLoader: MockWeatherLoader!
+    
+    override func setUpWithError() throws {
+        weatherLoader = MockWeatherLoader()
+        viewModel = WeatherListViewModel(weatherLoader: weatherLoader)
+        viewController = WeatherListViewController(viewModel: viewModel)
+        viewController.loadViewIfNeeded()
+    }
+    
+    override func tearDownWithError() throws {
+        viewController = nil
+        viewModel = nil
+    }
+    
+    func test_tableViewDataSource() {
+        XCTAssertNotNil(viewController.tableView.dataSource)
+        XCTAssertTrue(viewController.tableView.dataSource is WeatherListViewController)
+    }
+    
+    func test_tableViewDelegate() {
+        XCTAssertNotNil(viewController.tableView.delegate)
+        XCTAssertTrue(viewController.tableView.delegate is WeatherListViewController)
+    }
+    
+    func test_tableViewHasCells() {
+        let cell = viewController.tableView.dequeueReusableCell(withIdentifier: WeatherListCell.identifier)
+        XCTAssertNotNil(cell)
+    }
+    
+    func test_tableViewCellCount_withNoItems() {
+        weatherLoader.mockResult = .success([])
+        
+        viewModel.fetch()
+        
+        XCTAssertEqual(viewController.tableView.numberOfRows(inSection: 0), 0)
+    }
+    
+    func test_tableViewCellCount_withTwoItems() {
+        weatherLoader.mockResult = .success([anyWeather(), anyWeather()])
+        
+        viewModel.fetch()
+        
+        XCTAssertEqual(viewController.tableView.numberOfRows(inSection: 0), 2)
+    }
+    
+    func test_tableViewCell() {
+        let weathers = [anyWeather(), anyWeather(), anyWeather()]
+        let tableView = viewController.tableView
+        weatherLoader.mockResult = .success(weathers)
+        
+        viewModel.fetch()
+       
+        for (i,visibleCell) in tableView.visibleCells.enumerated() {
+            let cell = cell(on: index(i), from: tableView)
+            let weather = weathers[i]
+            
+            XCTAssertEqual(cell?.cityLabel.text, weather.city)
+            XCTAssertEqual(cell?.countryLabel.text, weather.country)
+            XCTAssertEqual(cell?.weatherDescriptionLabel.text, weather.weatherDescription.rawValue.capitalizedFirstLetterOfEachWord())
+            XCTAssertEqual(cell?.humidityInfoView.text, String(weather.humidity))
+            XCTAssertEqual(cell?.windSpeedInfoView.text, String(weather.windSpeed))
+            XCTAssertEqual(cell?.temperatureLabel.text, String(weather.temperature))
+        }
+    }
+    
+    // MARK: Helpers
+    private func cell(on indexPath: IndexPath, from tableView: UITableView) -> WeatherListCell? {
+        return tableView.cellForRow(at: indexPath) as? WeatherListCell
+    }
+    
+    private func index(_ index: Int) -> IndexPath {
+        IndexPath(row: index, section: 0)
+    }
+    
+}
